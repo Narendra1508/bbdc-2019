fd23e7f642d2cd45b9fee8539096c432975151ac
Narendra1508 <narendr.1508@gmail.com>
2020-05-01 18:04:13 +0200

    updated feature generator module

--- src/feature_generator.py
+++ src/feature_generator.py
@@ -1,4 +1,5 @@
 import os
+import errno
 import warnings  # To ignore any warnings warnings.filterwarnings("ignore")
 from glob import glob  # glob uses the wildcard pattern to create an iterable object file names # containing all matching file names in the current directory.
 
@@ -6,22 +7,39 @@ import numpy as np  # For mathematical calculations
 import pandas as pd  # For Pandas DataFrame
 from scipy.stats import kurtosis, skew  # To calculate skewness, kurtosis
 
+import pickle # To store the necessary files for efficient reuse
 
-def main(train_label_df, feature):
-    # files: to hold list of all datafiles to be read 
-    folders=[]
-    for sub in train_label_df.Datafile:
-        folders.append(sub.split('/')[0])
+def main(train_label_df, feature, data_flag):
+    #list all subject's data floder
+    folders = os.listdir("./../dataset/" + data_flag)
+    
+    df = pd.DataFrame()
 
     for folder in set(folders):
-        # read all the training datasets one by one for each subject
-        train_data_df = pd.DataFrame(fetch_train_data(folder , feature))
+        # read all the datasets one by one for each subject in the directory listing
+        data_df = pd.DataFrame(fetch_train_data(folder , feature))
         
         # add the labels to training data
-        train_data_df['activity'] = train_label_df.loc[train_label_df.Subject == folder]['Label'].to_list()
+        if(data_flag == 'train'):
+            data_df['activity'] = train_label_df.loc[train_label_df.Subject == folder]['Label'].to_list()
         
-        # write the file with extracted features 
-        train_data_df.to_csv("dataset/processed_train/"+ folder +"_"+ feature +".csv", index=False)
+        # append each subject's dataframe to a common dataframe
+        df = df.append(data_df, ignore_index=True)
+
+    # random shuffling of all test/train data
+    df = df.sample(frac=1).reset_index(drop=True)
+
+    # write the file with extracted features
+    out_file = "./../dataset/pickle/"+ data_flag +"/"+ feature +".pickle"
+    if not os.path.exists(os.path.dirname(out_file)):
+        try:
+            os.makedirs(os.path.dirname(out_file))
+        except OSError as exc: # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
+
+    pickle_out = open(out_file,"wb")
+    pickle.dump(df, pickle_out)
 
     print("Excuted Successfully")
 
@@ -31,7 +49,7 @@ def fetch_train_data(folder, feature):
     dataframe = pd.DataFrame()
     
     # filenames: holds all the activity files given subject
-    file_names = glob("dataset/train/" + folder +"/*.csv")
+    file_names = glob("./../dataset/" + data_flag + "/" + folder +"/*.csv")
     
     #read each activity file of the subject
     for file_name in file_names:
@@ -69,9 +87,13 @@ def time_stats(switcher_df, switcher_feature):
     
 if __name__ == '__main__':
     # read labled training data
-    train_label_df = pd.read_csv("dataset/train.csv")
+    train_label_df = pd.read_csv("./../dataset/train.csv")
     
     # Select required feature from the below set
     # {'mean','median','min','max','std','variance','mad','rms','zcr','iqr','pe','kurtosis','skew'}
-    feature = 'std'
-    main(train_label_df, feature)
+    feature = 'mean'
+    
+    # flag is to determine test and train data
+    data_flag = 'train'
+    
+    main(train_label_df, feature, data_flag)
